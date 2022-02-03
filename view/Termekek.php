4c6cbb68f505da829abd6e25c0d015f6f4ab3f9f
Orosz01 <gazsi1@hotmail.com>
2022-02-03 09:22:33 +0100

    Majdnem kész kinézet

--- view/Termekek.php
+++ view/Termekek.php
@@ -1,18 +1,19 @@
-<?php
-echo "Termékek"; 
-?>
-
-
 <body class="Te">
-  <div id="mySidebar" class="sidebar">
+  <div id="mySidenav" class="sidenav">
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="#">Pénznemek</a>
-        <a href="#">Tazók</a>
-        <a href="#">Kártyák</a>
-        <a href="#">Egyéb termékek</a>
+        <a href="P">Pénznemek</a>
+        <a href="T">Tazók</a>
+        <a href="K">Kártyák</a>
+        <a href="E">Egyéb termékek</a>
       </div>
-      
-      <div id="main">
-        <button class="openbtn" onclick="openNav()">&#9776; Open Sidebar</button>
-      </div>
+      <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Termékek</span>
+      <script>
+        function openNav() {
+          document.getElementById("mySidenav").style.width = "200px";
+        }
+
+        function closeNav() {
+          document.getElementById("mySidenav").style.width = "0px";
+        }
+      </script>
 </body>
