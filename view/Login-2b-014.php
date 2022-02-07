333ac4336a908dbc6e4ba506134604dad02ef7bb
Orosz01 <gazsi1@hotmail.com>
2022-02-07 16:57:11 +0100

    asd

---
+++ view/Login-2b-014.php
@@ -0,0 +1,9 @@
+<body class="Lo">
+    <form class="Be">
+        <label for="username">Felhasználó név:</label><br>
+        <input type="text" class="area" id="username" name="username"><br>
+        <label for="password">Jelszó:</label><br>
+        <input type="text" class="area" id="password" name="password"><br><br>
+        <input type="submit" id="submit" value="Bejelentkezés"> 
+    </form>
+</body>
