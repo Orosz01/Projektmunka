ef2d62f8fe4387e5bb3f6c971533975da39ffc8c
Orosz01 <gazsi1@hotmail.com>
2022-02-07 17:09:18 +0100

    vmi galiba tortent

--- controller/reg-2b-014.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include "view/Reg.php"
-
-?>
--- view/Fooldal-2b-014.php
+++
@@ -1,5 +0,0 @@
-<body class="Fo">
-    
-</body>
-<h1>Üdv a gyűjtőknek szánt keresekedő oldalon </h1>
-<p>Remélem megtalálod azt amit keresel vagy épp meg tudsz válni valamitől ami már nem fontos számodra, s ezzel gyarapítod mások gyűjteményét.</p>
