f228df9ebfd9ee27f475819d8b874088ebd4935f
Orosz01 <gazsi1@hotmail.com>
2022-02-24 09:26:59 +0100

    Termekek oldalsáv moove

--- view/Termekek.php
+++ view/Termekek.php
@@ -1,20 +1,29 @@
 <body class="Te">
   <div id="mySidenav" class="sidenav">
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="index.php?page=termekek&action=Penzem">Pénznemek</a>
-        <a href="T">Tazók</a>
-        <a href="K">Kártyák</a>
-        <a href="E">Egyéb termékek</a>
+        <a href="index.php?page=termekek&action=Penznem">Pénznemek</a>
+        <a href="index.php?page=termekek&action=Tazok">Tazók</a>
+        <a href="index.php?page=termekek&action=Kartyak">Kártyák</a>
+        <a href="index.php?page=termekek&action=Egyeb_Dolgok">Egyéb termékek</a>
       </div>
       <span style="font-size:30px;cursor:pointer" id="oldalsav" onclick="openNav()">&#9776; Termékek</span>
       <?php 
         if (isset($_REQUEST['action'])){
-          if($_REQUEST['action']=="Penzem"){
-            ?>
-              
-            <?php
-          } 
-        }
+        if($_REQUEST['action']=="Penznem"){
+          }
+          }
+        if (isset($_REQUEST['action'])){
+        if($_REQUEST['action']=="Tazok"){
+          }
+          }
+        if (isset($_REQUEST['action'])){
+        if($_REQUEST['action']=="Kartyak"){
+          }
+          }
+        if (isset($_REQUEST['action'])){
+        if($_REQUEST['action']=="Egyeb_dolgok"){
+          }
+          }
       ?>
       <script>
         function openNav() {
