832d8bccab2d7510cc5299ef485c2a7a1ed44c19
Orosz01 <gazsi1@hotmail.com>
2022-03-01 10:44:52 +0100

    feltoltes gombok

--- controller/feltoltes.php
+++ controller/feltoltes.php
@@ -1,5 +1,4 @@
 <?php
-
 $i = 0;
 $errors = array();
 if(isset($_FILES["fileToUpload"])){
@@ -23,9 +22,31 @@ foreach($_FILES["fileToUpload"]["name"] as $key => $name){
     }
 }
 ?>
+
         <?php
         
 
 include 'view/Feltoltes.php';
 
 ?>
+<script>
+  $(".hide1").hide();
+  $(".hide2").hide();
+  $(".hide3").hide();
+
+  $("#Penznem").click(function(){
+  $(".hide1").show();
+  $(".hide2").hide();
+  $(".hide3").hide();
+});
+$("#Kartya").click(function(){
+  $(".hide2").show();
+  $(".hide1").hide();
+  $(".hide3").hide();
+});
+$("#Tazok").click(function(){
+  $(".hide3").show();
+  $(".hide2").hide();
+  $(".hide1").hide();
+});
+</script>
--- view/Feltoltes.php
+++ view/Feltoltes.php
@@ -5,42 +5,38 @@
         <label for="termeknev">Esetleges eladási ár:</label><br>
         <input type="text" class="area"  name="Ar"><br>
         <p>Válaszd ki a terméked kategóriáját</p>
-        <input type="radio" id="" name="Kat" value="Penznem">
+
+        <input type="radio" id="Penznem" name="Kat" value="Penznem">
         <label for="Penznem">Pénznemek</label>
-        <input type="radio" id="" name="Kat" value="Kartya">
+        <input type="radio" id="Kartya" name="Kat" value="Kartya">
         <label for="Kartya">Kártyák</label>
-        <input type="radio" id="" name="Kat" value="Tazok">
+        <input type="radio" id="Tazok" name="Kat" value="Tazok">
         <label for="Tazok">Tazók</label>
         <input type="radio" id="" name="Kat" value="Egyeb dolgok">
         <label for="Egyeb Dolgok">Egyéb Dolgok</label><br>
-        <input type="radio" id="" name="Al_Kat" value="Pengo">
-        <label for="Pengo">Pengő</label>
-        <input type="radio" id="" name="Al_Kat" value="Erem">
-        <label for="Erme">Érme</label><br>
-        <input type="radio" id="" name="Al_Kat" value="Yu-Gi-Oh">
-        <label for="Yu-Gi-Oh">Yu-Gi-Oh</label>
-        <input type="radio" id="" name="Al_Kat" value="Kungfu Panda">
-        <label for="Kungfu Panda">Kungfu Panda</label>
-        <input type="radio" id="" name="Al_Kat" value="Focis">
-        <label for="Focis">Focis</label><br>
-        <input type="radio" id="" name="Al_Kat" value="Angry Birds">
-        <label for="Angry Birds">Angry Birds</label>
-        <input type="radio" id="" name="Al_Kat" value="Pokemon">
-        <label for="Pokemon">Pokemon</label><br>
 
+        <div class="hide1">
+        <input type="radio" id="money" name="Al_Kat" value="Pengo">
+        <label for="Pengo">Pengő</label>
+        <input type="radio" id="money" name="Al_Kat" value="Erem">
+        <label for="Erme">Érme</label><br>
+        </div>
+        <div  class="hide2">
+        <input type="radio" id="Kartyak" name="Al_Kat" value="Yu-Gi-Oh">
+        <label for="Yu-Gi-Oh">Yu-Gi-Oh</label>
+        <input type="radio" id="Kartyak" name="Al_Kat" value="Kungfu Panda">
+        <label for="Kungfu Panda">Kungfu Panda</label>
+        <input type="radio" id="Kartyak" name="Al_Kat" value="Focis">
+        <label for="Focis">Focis</label><br>
+        </div>
+        <div  class="hide3">
+        <input type="radio" id="taz" name="Al_Kat" value="Angry Birds">
+        <label for="Angry Birds">Angry Birds</label>
+        <input type="radio" id="taz" name="Al_Kat" value="Pokemon">
+        <label for="Pokemon">Pokemon</label><br>
+        </div>
             Válaszd ki a feltöltendő termék képét:<br>
             <input type="file" name = "fileToUpload[]" id = "fileToUpload" id="uplo"><br>
             <div class="upi"><input type ="submit" value ="Termék feltöltése" name="submit" id="uplo"><br></div>
-        <?php
-            if($i > 0) echo " A termék feltöltve<br>";
-            if($errors){
-                foreach($errors as $error){
-                    foreach($error as $errorMsg){
-                        echo "$errorMsg <br>";
-                    }
-                }
-            }
-        ?>
         </form>
-
 </body>
