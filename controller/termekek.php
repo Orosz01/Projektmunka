f90007fb6e688672f2b730cd2eef1ab18c005830
Orosz01 <gazsi1@hotmail.com>
2022-04-15 15:32:05 +0200

    rendezes

--- controller/termekek.php
+++ controller/termekek.php
@@ -1,69 +1,60 @@
 <?php
-
 if(isset($_POST['uzi'])){
-$q="";
-$ro="";
-$man="";
-$satek="";
-$ra="";
-if($_POST['mainkat']==1){
-    $q="kartyak";
-    $man=$kartya;
-    $satek="set_kartya";
-}else if($_POST['mainkat']==2){
-    $q="penznem";
-    $man=$penz;
-    $satek="set_penz";
-}else if($_POST['mainkat']==3){
-    $q="tazok";
-    $man=$tazo;
-    $satek="set_tazo";
-}else if($_POST['mainkat']==4){
-    $q="egyeb_termekek";
-    $man=$egyeb;
-    $satek="set_egyeb";
-}
+    $kat="";
+    $termek="";
+    $main="";
+    $satek="";
+   $celemail=""; 
+    if($_POST['mainkat']==1){
+    $kat="kartyak";
+       $main=$kartya;
+        $satek="set_kartya";
+        }else if($_POST['mainkat']==2){
+    $kat="penznem";
+       $main=$penz;
+        $satek="set_penz";
+        }else if($_POST['mainkat']==3){
+    $kat="tazok";
+       $main=$tazo;
+        $satek="set_tazo";
+        }else if($_POST['mainkat']==4){
+    $kat="egyeb_termekek";
+       $main=$egyeb;
+        $satek="set_egyeb";
+        }
 
-$sql="Select email from felhasznalok where F_Id=(SELECT F_Id FROM ".$q." Where Termek_azonosito=".$_POST['mainkat']." and ".$q.".id=".$_POST['termekid'].")";
-
-if(!$result = $conn->query($sql)) echo $conn->error;
-if ($result->num_rows > 0) {
- 
-      
-    foreach($result->fetch_assoc() as $key){
-        $ra=$key;
+    $sql="Select email from felhasznalok where F_Id=(SELECT F_Id FROM ".$kat." Where Termek_azonosito=".$_POST['mainkat']." and ".$kat.".id=".$_POST['termekid'].")";
+    if(!$result = $conn->query($sql)) echo $conn->error;
+    if ($result->num_rows > 0) {
+        foreach($result->fetch_assoc() as $key){
+           $celemail=$key;
+        }
+        
     }
-    
+
+    $sql="Select Termek_nev from ".$kat." where ".$kat.".id=".$_POST['termekid']."";
+    if(!$result = $conn->query($sql)) echo $conn->error;
+
+    if ($result->num_rows > 0) {
+        
+    $termekasd=$man->get_Termek_nev();
+        foreach($result->fetch_assoc() as $key);
+    $termek=$key;
+    }
+    // the message
+    $msg = $_SESSION['username']. " Szeretne üzletet kötni önnel a/az ".$termek." termékkel kapcsolatban <br> <a href='http://banki13.komarom.net/zsolt/szakdolgozat/index.php'>Az oldalra</a>";
+    $header = 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    // use wordwrap() if lines are longer than 70 characters
+    $msg = wordwrap($msg,70);
+
+    $header .= "From: ".$_SESSION['email'];
+
+    // send email
+    mail($celemail ,"Csere / Üzleti ajánlat",$msg,$header);
 }
-
-
-$sql="Select Termek_nev from ".$q." where ".$q.".id=".$_POST['termekid']."";
-if(!$result = $conn->query($sql)) echo $conn->error;
-
-if ($result->num_rows > 0) {
-    
-    $roasdfsd=$man->get_Termek_nev();
-    foreach($result->fetch_assoc() as $key);
-    $ro=$key;
-}
-// the message
-$msg = $_SESSION['username']. " Szeretne üzletet kötni önnel a/az ".$ro." termékkel kapcsolatban <br> <a href='http://banki13.komarom.net/zsolt/szakdolgozat/index.php'>Az oldalra</a>";
-$header = 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
-
-$header .= "From: ".$_SESSION['email'];
-
-// send email
-mail( $ra ,"Csere / Üzleti ajánlat",$msg,$header);
-
-}
-
 $egyebid=$egyeb->egyeble($conn);
 $kartyaid=$kartya->kartyale($conn);
 $penzid=$penz->penzle($conn);
 $tazoid=$tazo->tazole($conn);
-
 include "view/Termekek.php"
-
 ?>
--- view/Fooldal.php
+++ view/Fooldal.php
@@ -1,18 +1,15 @@
 <body id="Fo">
+    
     <h1>Üdv a gyűjtőknek szánt keresekedő oldalon </h1>
     <p>Remélem megtalálod azt amit keresel vagy épp meg tudsz válni valamitől ami már nem fontos számodra, s ezzel gyarapítod mások gyűjteményét.</p>
 
     <?php
     if(isset($_SESSION['F_Id'])){
     ?>
-
-    
     <form action="index.php?page=fooldal" method="post" enctype="multipart/form-data" id="alchat">
             <?php
                 $a=0;
                 echo '<div id="uzik" style="overflow:scroll">';
-                
-               
                 echo '</div>';
             ?>
           <div id="bevit">
@@ -24,31 +21,21 @@
     <?php
     }
     ?>
-   
     <script>
         load_chat();
-        
         function gorgetes(){
             var iHeight = $("#uzik").prop("scrollHeight");
             $(document).ready(function() {
                 document.getElementById("uzik").scrollTop =iHeight;
             });
         }
-
-
         function load_chat(){
             $.ajax({
-
                 url:"controller/chatlekeres.php",
-
                 method:"POST",
-
                 data:{action:'chat'},
-
                 dataType:"JSON",
-
                 success:function(data)
-
                 {
                     var szoveg="";
                     for(i=0;i<data.length;i++){
@@ -57,11 +44,8 @@
                     $("#uzik").html(szoveg);
                     gorgetes();
                 }  
-
             })
-
         }
         setInterval(load_chat,3000);
-
         </script>
 
